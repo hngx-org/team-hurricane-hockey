ba83f6f36f1fb9a59d12247e9a590512ad052215
chizzykas <chizzykas@gmail.com>
2023-10-10 18:31:20 +0100

    Splash Screen and Home Menu Created

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:team_hurricane_hockey/screens/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:team_hurricane_hockey/router/app_router.dart';
+import 'package:team_hurricane_hockey/router/base_navigator.dart';
+import 'package:team_hurricane_hockey/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +15,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      navigatorKey: BaseNavigator.key,
       title: 'Hurricane Hockey',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         dividerColor: Colors.yellowAccent,
         dividerTheme: const DividerThemeData(space: 1),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 6.0),
+            side: const BorderSide(
+              color: Colors.white,
+              width: 2.0,
+            ),
+            shape: const BeveledRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12.0),
+                bottomRight: Radius.circular(12.0),
+              ),
+            ),
+          ),
+        ),
+        textTheme: TextTheme(
+          headlineLarge: GoogleFonts.pressStart2p(
+            fontSize: 40.0,
+            color: Colors.white,
+            // fontWeight: FontWeight.w900,
+          ),
+          labelMedium: GoogleFonts.tektur(
+            fontSize: 32.0,
+            color: Colors.white,
+          ),
+        ),
       ),
-      home: const MyHomePage(),
+      onGenerateRoute: AppRouter.onGenerateRoute,
+      initialRoute: SplashScreen.routeName,
     );
   }
 }
