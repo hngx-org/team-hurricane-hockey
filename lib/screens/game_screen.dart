2b0b908493e04bb4a8554220a05a6d91198d80a3
chizzykas <chizzykas@gmail.com>
2023-10-16 18:53:59 +0100

    Fix

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -347,7 +347,8 @@ class _MyHomePageState extends State<GameScreen> {
     showStartText = true;
 
     if ((widget.gameMode == GameMode.ai && player1.score == gameEndsAt) ||
-        (widget.gameMode == GameMode.player2 && player1.score == gameEndsAt)) {
+        (widget.gameMode == GameMode.player2 && player1.score == gameEndsAt)||
+        (widget.gameMode == GameMode.multiplayer && player1.score == gameEndsAt)) {
       blowFinalWhistle();
       turn = player1.name;
       gameIsFinished = true;
@@ -361,7 +362,8 @@ class _MyHomePageState extends State<GameScreen> {
       // textStartFontSize *= 2;
     } else if ((widget.gameMode == GameMode.ai &&
             player2.score == gameEndsAt) ||
-        (widget.gameMode == GameMode.player2 && player2.score == gameEndsAt)) {
+        (widget.gameMode == GameMode.player2 && player2.score == gameEndsAt)||
+        (widget.gameMode == GameMode.multiplayer && player2.score == gameEndsAt)) {
       blowFinalWhistle();
       gameIsFinished = true;
       return showDialog(
